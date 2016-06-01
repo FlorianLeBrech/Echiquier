2efb409406d4a6511b83360d7bd682cbd5637c99
Florian <Florian@10.170.1.26>
2016-06-01 11:34:32 +0200

    Modification Abstract piece

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -9,8 +9,46 @@ package model;
  *
  * @author Florian
  */
-abstract class AbstractPiece implements Pieces{
-   
-    
-    
+abstract class AbstractPiece implements Pieces {
+
+    private Coord coord;
+    private Couleur couleur;
+
+    public AbstractPiece(Couleur couleur, Coord coord) {
+        this.couleur = this.couleur;
+        this.coord = coord;
+        capture = false;
+    }
+
+    public int getX() {
+        return this.coord.x;
+    }
+
+    public int getY() {
+        return this.coord.y;
+    }
+
+    public Couleur getCouleur() {
+        return this.couleur;
+    }
+
+    public java.lang.String getName() {
+        return this.getClass().toString();      // a changer peut etre 
+    }
+
+    public boolean move(int x, int y) {
+        this.coord.x = x;
+        this.coord.y = y;
+        return true;
+    }
+
+    public boolean capture() {
+        this.coord.x = -1;
+        this.coord.y = -1;
+        return true;
+
+    }
+
+    public abstract boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible);
+
 }
