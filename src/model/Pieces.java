964400a4fc0da00ca659f90a8d3a2dbb0d51c072
hugo.talbot <hugo.talbot@TPME16.etudiant.cpe.fr>
2016-06-01 11:09:58 +0200

    remplissage de l'interface Pieces

--- src/model/Pieces.java
+++ src/model/Pieces.java
@@ -10,6 +10,20 @@ package model;
  *
  * @author hugo.talbot
  */
-public class Pieces {
+public interface Pieces {
+    
+    public boolean capture();
+           
+    public Couleur getCouleur();
+     
+    public String getName();
+      
+    public int getX();
+    
+    public int getY();
+     
+    public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible);
+      
+    public boolean move(int xFinal, int yFinal);
     
 }
