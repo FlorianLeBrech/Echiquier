a50f358038b954269da7ee1e1f7f7ce4bd3250e4
hugo.talbot <hugo.talbot@TPME16.etudiant.cpe.fr>
2016-06-01 14:23:07 +0200

    la classe Jeu

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -16,26 +16,22 @@ import tools.ChessPiecesFactory;
  * @author hugo.talbot
  */
 public class Jeu implements Game {
-    List<Pieces> pieces = new LinkedList<>();
-    public Jeu() {
-        this.pieces=ChessPiecesFactory.newPieces(Couleur.BLANC);
+    Couleur couleur;
+    List<Pieces> pieces;
+    public Jeu(Couleur couleur) {
+        this.pieces=ChessPiecesFactory.newPieces(couleur);
     }
     
     public static void main (String[] args){
-        Jeu jeu = new Jeu();
-        
+        Jeu jeu = new Jeu(Couleur.BLANC);
         System.out.println(jeu);
     }
-    
+
     @Override
     public String toString() {
-        Iterator it = pieces.iterator();
-        while(it.hasNext()) {
-            System.out.println(it);
-        }
-        return "";
+        return "Jeu{" + "pieces=" + pieces + '}';
     }
-    
+     
     @Override
     public boolean isPieceHere(int x, int y) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
