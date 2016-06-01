c4a821b4147cfbb7cb3304a4720a69e2b635f4fa
hugo.talbot <hugo.talbot@TPME16.etudiant.cpe.fr>
2016-06-01 17:32:33 +0200

    tout

--- src/controler/controlerLocal/ChessGameControler.java
+++ src/controler/controlerLocal/ChessGameControler.java
@@ -26,7 +26,8 @@ public class ChessGameControler extends AbstractChessGameControler {
 	 */
 	@Override
 	public boolean isPlayerOK(Coord initCoord) {
-            return false;
+       //     return this.chessGame.getColorCurrentPlayer().
+            return true;
 		// ToDo
 	}
 	
