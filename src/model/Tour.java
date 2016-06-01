3196520478e95b369f63ac9d99924ac9ede3677d
hugo.talbot <hugo.talbot@TPME16.etudiant.cpe.fr>
2016-06-01 11:26:14 +0200

    la classe Tour

---
+++ src/model/Tour.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author hugo.talbot
+ */
+public class Tour extends AbstractPiece {
+    Couleur couleur;
+    Coord coord;
+    
+    Tour(Couleur couleur, Coord coord) {
+        super(couleur, coord);
+    }
+            
+    public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
+        if(this.x==xFinal || this.y==yFinal) {
+            if(!isCatchOk) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
