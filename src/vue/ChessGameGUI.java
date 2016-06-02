df677f108dabba96095c555c305ddc2644bb0bf0
Florian <Florian@10.170.1.26>
2016-06-02 10:50:50 +0200

    Modification flo

--- src/vue/ChessGameGUI.java
+++ src/vue/ChessGameGUI.java
@@ -3,27 +3,64 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package vue;
 
 import controler.ChessGameControlers;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 /**
  *
  * @author hugo.talbot
  */
-public class ChessGameGUI extends JFrame{
+public class ChessGameGUI extends JFrame {
 
-     String jeu_echec;
-     ChessGameControlers chessGameControler;
-     Dimension dim;
+    String jeu_echec;
+    ChessGameControlers chessGameControler;
+    Dimension dim;
+
+    private JLayeredPane layeredPane;
+    private JPanel chessBoard;
+    private JLabel chessPiece;
+    private int xAdjustment;
+    private int yAdjustment;
+
     public ChessGameGUI(String jeu_déchec, ChessGameControlers chessGameControler, Dimension dim) {
-    this.chessGameControler=chessGameControler;
-    this.jeu_echec=jeu_déchec;
-    this.dim=dim;
-    
+        this.chessGameControler = chessGameControler;
+        this.jeu_echec = jeu_déchec;
+        this.dim = dim;
+
+        
+        layeredPane = new JLayeredPane();
+  getContentPane().add(layeredPane);
+  layeredPane.setPreferredSize(dim);
+  //layeredPane.addMouseListener(this);
+  //layeredPane.addMouseMotionListener(this);
+        chessBoard = new JPanel();
+        layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+        chessBoard.setLayout(new GridLayout(8, 8));
+        chessBoard.setPreferredSize(this.dim);
+        chessBoard.setBounds(0, 0, this.dim.width, this.dim.height);
+        for (int i = 0; i < 64; i++) {
+            JPanel square = new JPanel(new BorderLayout());
+            chessBoard.add(square);
+
+            int row = (i / 8) % 2;
+            if (row == 0) {
+                square.setBackground(i % 2 == 0 ? Color.blue : Color.white);
+            } else {
+                square.setBackground(i % 2 == 0 ? Color.white : Color.blue);
+            }
+        }
+        
+        
+
     }
-    
+
 }
